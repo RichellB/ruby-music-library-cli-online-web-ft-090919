0a03f56f3c2b0dc6bbdef5727332839f5e833280
Richell Bonfield <richell.bonfield12@gmail.com>
2019-09-30 01:26:29 +0000

    Automatically backed up by Learn

---
+++ lib/genre.rb
@@ -0,0 +1,35 @@
+class Genre 
+  extend Concerns::Findable
+  attr_accessor :name
+  attr_reader :songs
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+   # save
+    @songs = []
+  end 
+  
+  def self. all 
+    @@all 
+  end
+  
+  def save 
+    @@all << self 
+  end
+  
+  def self.destroy_all 
+    @@all.clear 
+  end
+  
+  def self.create(name) 
+    genre = Genre.new(name) 
+    genre.save
+    genre
+  end
+  
+  def artists
+    songs.collect {|sg| sg.artist }.uniq
+  end
+end

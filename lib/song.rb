22ff5ba5fb5b0451c6d0801eea3010c27bafeec1
Richell Bonfield <richell.bonfield12@gmail.com>
2019-09-30 01:32:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,6 +11,17 @@ class Song
     self.genre = genre if genre
   end 
   
+   
+   def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
+
+  def genre=(genre)
+    @genre = genre
+    genre.songs << self unless genre.songs.include?(self)
+  end
+  
   def self. all 
     @@all 
   end
